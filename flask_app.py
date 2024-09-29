097b826fe0fb9dc4dc9f8d484768bb31e10fbfae
MoejoeMan <joehayes006@gmail.com>
2024-09-29 15:55:03 +0000

    test

--- flask_app.py
+++ flask_app.py
@@ -1,32 +1,22 @@
-from flask import Flask, redirect, render_template, request, url_for
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="Moejoe06",
-    password="FZ$7uYuCHTCx7wm",
-    hostname="Moejoe06.mysql.pythonanywhere-services.com",
-    databasename="Moejoe06$Comments",
-)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
 
-class Comment(db.Model):
-
-    __tablename__ = "comments"
-
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(4096))
-comments = []
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    if request.method == "GET":
-        return render_template("main_page.html", comments=comments)
+    return render_template("main_page.html")
 
-    comments.append(request.form["contents"])
-    return redirect(url_for('index'))
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run()
